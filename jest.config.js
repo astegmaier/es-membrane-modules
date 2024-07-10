2f8053f0e0d820a7ff70a44f9e25fb33e951c043
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2024-07-10 17:42:34 -0400

    Add ecma tests.

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,8 @@ const config = {
     "^.+\\.(t|j)sx?$": "@swc/jest",
   },
   globals: {
-    "DogfoodMembrane": undefined
+    "DogfoodMembrane": undefined, // TODO: we might have to make this a part of the code itself, not just the tests - check on this.
+    fail: (error) => {throw new Error(error);}, // The es-membrane tests were originally written for jasmine, which provides a global fail function (unlike jest).
   }
 };
 
---
+++ test/ecma/iterators.test.js
@@ -0,0 +1,53 @@
+import { Membrane } from "../../src";
+
+it("Iterators through a membrane work as expected", function() {
+  let parts = {
+    wet: {
+      iterator: {
+        count: 0
+      }
+    },
+    dry: {},
+    handlers: {},
+    membrane: new Membrane(),
+
+    response: { value: true }
+  };
+
+  parts.wet.iterator[Symbol.iterator] = function() {
+    return {
+      next: function() {
+        let rv = {
+          value: { count: this.count },
+          done: this.count > 3
+        };
+        this.count++;
+        return rv;
+      },
+      get count() {
+        return parts.wet.iterator.count;
+      },
+      set count(val) {
+        parts.wet.iterator.count = val;
+        return true;
+      }
+    };
+  };
+
+  parts.handlers.wet = parts.membrane.getHandlerByName(
+    "wet", { mustCreate: true }
+  );
+  parts.handlers.dry = parts.membrane.getHandlerByName(
+    "dry", { mustCreate: true }
+  );
+
+  parts.dry.iterator = parts.membrane.convertArgumentToProxy(
+    parts.handlers.wet,
+    parts.handlers.dry,
+    parts.wet.iterator
+  );
+
+  let items = Array.from(parts.dry.iterator)
+                   .map((val) => val.count);
+  expect(items).toEqual([0, 1, 2, 3]);
+});
