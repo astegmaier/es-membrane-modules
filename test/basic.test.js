aa0601d3820d7b26f90e2c1af0e6bc69407d46ce
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2024-07-10 10:52:14 -0400

    Scaffold jest with swc for esm support.

---
+++ test/basic.test.js
@@ -0,0 +1,6 @@
+import { ImportedThing } from "./importedThing";
+
+it("does something with ImportedThing", () => {
+    const importedThing = new ImportedThing();
+    expect(importedThing.foo).toBe("bar");
+})
---
+++ test/importedThing.js
@@ -0,0 +1,3 @@
+export class ImportedThing {
+    foo = "bar";
+}
