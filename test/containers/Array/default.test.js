646eb1192d66997baf785f45fee905673cd97ae1
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2024-07-10 20:44:07 -0400

    Port containers tests.

---
+++ test/containers/Array/default.test.js
@@ -0,0 +1,63 @@
+describe.skip("Arrays by default in a membrane", function () {
+  let originalArray;
+  function buildElement(name) {
+    let rv = { name };
+    Object.freeze(rv);
+    return rv;
+  }
+  const wetAlpha = buildElement("alpha"),
+    wetBeta = buildElement("beta"),
+    wetGamma = buildElement("gamma"),
+    wetPi = buildElement("pi"),
+    wetChi = buildElement("chi");
+  beforeEach(function () {
+    originalArray = [wetAlpha, wetBeta, wetGamma, wetPi, wetChi];
+  });
+
+  it.skip("All array methods still work", () => {
+    /* Array.prototype has a lot of methods.  We can group them into types:
+   * Modifiers:
+   *   pop()
+   *   push()
+   *   shift()
+   *   unshift()
+   *   splice()
+   *   sort()
+   *   reverse()
+
+   * Copies:
+   *   slice()
+   *   concat()
+   *   join()
+
+   * Searches:
+   *   includes()
+   *   indexOf()
+   *   lastIndexOf()
+   *   entries()
+   *   find()
+   *   findIndex()
+
+   * Iteration over elements
+   *   every()
+   *   some()
+   *   filter()
+   *   forEach()
+   *   keys()
+   *   values()
+   *   map()
+   *   reduce()
+   *   reduceRight()
+
+   * Object.prototype overrides
+   *   toLocaleString()
+   *   toSource()
+   *   toString()
+
+   *   copyWithin()
+   *   fill()
+   *   [@@iterator]()
+   * 
+   */
+  });
+});
