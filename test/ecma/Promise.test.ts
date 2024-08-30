8f7748105b7e8df026a194035001a679732b6e87
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2024-08-30 09:12:26 -0400

    Convert Promise.test to typescript

--- test/ecma/Promise.test.ts
+++ test/ecma/Promise.test.ts
@@ -1,25 +1,48 @@
 import { Membrane } from "../../src";
+import type { ObjectGraphHandler } from "../../src";
+
+interface IResponse {
+  value: true;
+}
+
+interface IPromiseGraph {
+  wrapper: {
+    promise: Promise<IResponse | void>;
+    resolve: (value: IResponse | PromiseLike<IResponse>) => void;
+    reject: (value: unknown) => void;
+  };
+}
+
+interface IPromiseMocks {
+  wet: IPromiseGraph;
+  dry: IPromiseGraph;
+  handlers: { wet: ObjectGraphHandler; dry: ObjectGraphHandler };
+  membrane: Membrane;
+  response: IResponse;
+}
 
 describe("Promises through a membrane", function () {
-  let parts;
+  let parts: IPromiseMocks;
   beforeEach(function () {
+    const membrane = new Membrane();
     parts = {
       wet: {
         wrapper: {}
+      } as IPromiseGraph,
+      dry: {} as IPromiseGraph,
+      handlers: {
+        wet: membrane.getHandlerByName("wet", { mustCreate: true }),
+        dry: membrane.getHandlerByName("dry", { mustCreate: true })
       },
-      dry: {},
-      handlers: {},
-      membrane: new Membrane(),
+      membrane,
 
       response: { value: true }
     };
+
     parts.wet.wrapper.promise = new Promise(function (resolve, reject) {
       parts.wet.wrapper.resolve = resolve;
       parts.wet.wrapper.reject = reject;
     });
-
-    parts.handlers.wet = parts.membrane.getHandlerByName("wet", { mustCreate: true });
-    parts.handlers.dry = parts.membrane.getHandlerByName("dry", { mustCreate: true });
 
     parts.dry.wrapper = parts.membrane.convertArgumentToProxy(
       parts.handlers.wet,
@@ -31,7 +54,7 @@ describe("Promises through a membrane", function () {
   it("may be resolved on the wet side (where the promise came from)", function (done) {
     expect(parts.dry.wrapper.promise).not.toBe(parts.wet.wrapper.promise);
     parts.dry.wrapper.promise = parts.dry.wrapper.promise.then(function (result) {
-      expect(result.value).toBe(true);
+      expect(result!.value).toBe(true);
     }, fail);
     parts.dry.wrapper.promise = parts.dry.wrapper.promise.then(done, done);
     parts.wet.wrapper.resolve(parts.response);
@@ -48,7 +71,7 @@ describe("Promises through a membrane", function () {
   it("may be resolved on the dry side", function (done) {
     expect(parts.dry.wrapper.promise).not.toBe(parts.wet.wrapper.promise);
     parts.dry.wrapper.promise = parts.dry.wrapper.promise.then(function (result) {
-      expect(result.value).toBe(true);
+      expect(result!.value).toBe(true);
     }, fail);
     parts.dry.wrapper.promise = parts.dry.wrapper.promise.then(done, done);
     parts.dry.wrapper.resolve(parts.response);
