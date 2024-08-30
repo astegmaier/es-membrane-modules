5a287769bc8b87c9d7cbb1f69430be2fc7d0e702
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2024-08-30 08:33:16 -0400

    Convert generators.test to typescript

--- test/ecma/generators.test.ts
+++ test/ecma/generators.test.ts
@@ -1,33 +1,44 @@
 import { Membrane } from "../../src";
+import type { ObjectGraphHandler } from "../../src";
+
+interface IGeneratorGraph {
+  buildGenerator(): Generator<{ count: number }, string, string>;
+}
+
+interface IGeneratorsMocks {
+  wet: IGeneratorGraph;
+  dry: IGeneratorGraph;
+  handlers: { wet: ObjectGraphHandler; dry: ObjectGraphHandler };
+  membrane: Membrane;
+  response: { value: true };
+}
 
 describe("Generators through a membrane", function () {
-  let parts;
+  let parts: IGeneratorsMocks;
+
   beforeEach(function () {
+    const buildGenerator = function* () {
+      let count = 0;
+      while (true) {
+        yield { count };
+        count++;
+      }
+    };
+    const membrane = new Membrane();
+    const wet = membrane.getHandlerByName("wet", { mustCreate: true });
+    const dry = membrane.getHandlerByName("dry", { mustCreate: true });
+
     parts = {
       wet: {
-        buildGenerator: function* () {
-          let count = 0;
-          while (true) {
-            yield { count };
-            count++;
-          }
-        }
+        buildGenerator
       },
-      dry: {},
-      handlers: {},
-      membrane: new Membrane(),
-
+      dry: {
+        buildGenerator: membrane.convertArgumentToProxy(wet, dry, buildGenerator)
+      },
+      handlers: { wet, dry },
+      membrane,
       response: { value: true }
     };
-
-    parts.handlers.wet = parts.membrane.getHandlerByName("wet", { mustCreate: true });
-    parts.handlers.dry = parts.membrane.getHandlerByName("dry", { mustCreate: true });
-
-    parts.dry.buildGenerator = parts.membrane.convertArgumentToProxy(
-      parts.handlers.wet,
-      parts.handlers.dry,
-      parts.wet.buildGenerator
-    );
   });
 
   it("work with normal stepping and a return call", function () {
