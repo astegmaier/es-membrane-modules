ed476034b8bbdd09bf38a80c774d203b65358f69
Andrew Stegmaier <andrew.stegmaier@microsoft.com>
2024-09-03 11:31:45 -0400

    Convert WeakSet test to typescript

--- test/containers/WeakSet/default.test.ts
+++ test/containers/WeakSet/default.test.ts
@@ -1,9 +1,16 @@
 import { Membrane } from "../../../src";
+import type { ObjectGraphHandler } from "../../../src";
 
 it("Set instances by default in a membrane work like they do without a membrane", function () {
   "use strict";
 
-  let membrane, wetHandler, dryHandler, dampHandler, wetSet, drySet, dampSet;
+  let membrane: Membrane,
+    wetHandler: ObjectGraphHandler,
+    dryHandler: ObjectGraphHandler,
+    dampHandler: ObjectGraphHandler,
+    wetSet: WeakSet<object>,
+    drySet: WeakSet<object>,
+    dampSet: WeakSet<object>;
   {
     const MUSTCREATE = Object.freeze({ mustCreate: true });
     membrane = new Membrane();
@@ -14,10 +21,10 @@ it("Set instances by default in a membrane work like they do without a membrane"
     wetSet = new WeakSet();
     drySet = membrane.convertArgumentToProxy(wetHandler, dryHandler, wetSet);
     // we rarely create proxies this way in our tests, so this'll be useful
-    dampSet = membrane.convertArgumentToProxy(dryHandler, dampHandler, drySet);
+    dampSet! = membrane.convertArgumentToProxy(dryHandler, dampHandler, drySet);
   }
 
-  function checkSet(set, values, shouldHave = true) {
+  function checkSet(set: WeakSet<object>, values: object[], shouldHave = true) {
     values.forEach(function (value) {
       expect(set.has(value)).toBe(shouldHave);
     });
